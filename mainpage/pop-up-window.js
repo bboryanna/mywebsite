5954e39ab2c29f150468194ebd108e5e678b26c9
Boryana <bobilom2007@gmail.com>
2025-11-29 21:38:14 +0200

    push

--- mainpage/pop-up-window.js
+++ mainpage/pop-up-window.js
@@ -1,5 +1,18 @@
 // When the user clicks on <div>, open the popup
-function myFunction() {
-  var popup = document.getElementById("myPopup");
+function StrawberryFunction() {
+  var popup = document.getElementById("StrawberryPopup");
   popup.classList.toggle("show");
 }
+
+function OrangeFunction() {
+  var popup = document.getElementById("OrangePopup");
+  popup.classList.toggle("show");
+}
+
+function KiwiFunction() {
+  var popup = document.getElementById("KiwiPopup");
+  popup.classList.toggle("show");
+}
+
+
+
